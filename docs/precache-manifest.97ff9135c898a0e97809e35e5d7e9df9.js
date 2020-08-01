0cd97fa8ca62c1619e8adb6dc072f80ee5c30e7d
Enrique Villamayor <kikevillamayor@gmail.com>
2020-08-01 16:53:45 -0400

    docs agregado

---
+++ docs/precache-manifest.97ff9135c898a0e97809e35e5d7e9df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a333935ac4ed58c8ff193f8f358a61d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9750122b81d758abb020",
+    "url": "/static/css/main.72b1e7cd.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9750122b81d758abb020",
+    "url": "/static/js/main.3bb00383.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
